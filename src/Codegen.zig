47d9ecee8e37f48391bed68c499c94697567a4bb
Carlo Baseggio <carlo.baseggio@metaframe.it>
2025-08-21 11:27:18 +0200

    Impl bitwise operations

--- src/Codegen.zig
+++ src/Codegen.zig
@@ -89,10 +89,148 @@ pub fn build(self: @This(), ir: Ir) !void {
                         },
                     }
                 },
+                .call => |call| {
+                    // try w.print("  ;function call\n", .{});
+                    const fnName = call.name;
+                    const registers = [_][]const u8{ "RDI", "RSI", "RDX", "RCX", "R8", "R9" };
+                    if (call.args.len <= registers.len) {
+                        for (call.args, 0..) |arg, i| {
+                            const reg = registers[i];
+                            switch (arg) {
+                                .variable => |offset| try w.print("  mov {s}, [rbp - {d}] \n", .{ reg, offset }),
+                                .integerLiteral => |value| try w.print("  mov {s}, {d} \n", .{ reg, value }),
+                                .dataLiteral => |value| try w.print("  mov {s}, data{d}+0 \n", .{ reg, value }),
+                            }
+                        }
+                        // TODO azzera il registo AL, serve per chiamara printf, non so se serve per tutte le funzioni extrn
+                        try w.print("  xor eax, eax\n", .{});
+                        try w.print("  call {s}\n", .{fnName});
+                    } else {
+                        @panic("Call a funzioni esterne supporta al massimo 6 argomenti");
+                    }
+                },
                 .prefix_neg => |prefix| {
                     try loadReg(w, "  rax", prefix.arg);
                     try w.print("  neg rax\n", .{});
                     try w.print("  mov [rbp - {d}], rax\n", .{prefix.offset});
+                },
+                .infix_or => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  or rax, [rbp - {d}]\n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  or rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
+                },
+                .infix_and => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  and rax, [rbp - {d}]\n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  and rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
+                },
+                .infix_bit_or => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  or rax, [rbp - {d}]\n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  or rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
+                },
+                .infix_bit_xor => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  xor rax, [rbp - {d}]\n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  xor rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
+                },
+                .infix_bit_and => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  and rax, [rbp - {d}]\n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  and rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
+                },
+                .infix_eq => |infix| {
+                    try w.print("  xor rdx, rdx\n", .{});
+                    try loadReg(w, "  rax", infix.lhs);
+                    try loadReg(w, "  rbx", infix.rhs);
+                    try w.print("  cmp rax, rbx\n", .{});
+                    try w.print("  sete dl\n", .{});
+                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
+                },
+                .infix_not_eq => |infix| {
+                    try w.print("  xor rdx, rdx\n", .{});
+                    try loadReg(w, "  rax", infix.lhs);
+                    try loadReg(w, "  rbx", infix.rhs);
+                    try w.print("  cmp rax, rbx\n", .{});
+                    try w.print("  setne dl\n", .{});
+                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
+                },
+                .infix_l => |infix| {
+                    try w.print("  xor rdx, rdx\n", .{});
+                    try loadReg(w, "  rax", infix.lhs);
+                    try loadReg(w, "  rbx", infix.rhs);
+                    try w.print("  cmp rax, rbx\n", .{});
+                    try w.print("  setl dl\n", .{});
+                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
+                },
+                .infix_le => |infix| {
+                    try w.print("  xor rdx, rdx\n", .{});
+                    try loadReg(w, "  rax", infix.lhs);
+                    try loadReg(w, "  rbx", infix.rhs);
+                    try w.print("  cmp rax, rbx\n", .{});
+                    try w.print("  setle dl\n", .{});
+                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
+                },
+                .infix_ge => |infix| {
+                    try w.print("  xor rdx, rdx\n", .{});
+                    try loadReg(w, "  rax", infix.lhs);
+                    try loadReg(w, "  rbx", infix.rhs);
+                    try w.print("  cmp rax, rbx\n", .{});
+                    try w.print("  setge dl\n", .{});
+                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
+                },
+                .infix_g => |infix| {
+                    try w.print("  xor rdx, rdx\n", .{});
+                    try loadReg(w, "  rax", infix.lhs);
+                    try loadReg(w, "  rbx", infix.rhs);
+                    try w.print("  cmp rax, rbx\n", .{});
+                    try w.print("  setg dl\n", .{});
+                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
+                },
+                .infix_shift_left => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  shl rax, [rbp - {d}] \n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  shl rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
+                },
+                .infix_shift_right => |infix| {
+                    const target_offset = infix.offset;
+                    try loadReg(w, "  rax", infix.lhs);
+                    switch (infix.rhs) {
+                        .variable => |rhs_offset| try w.print("  shr rax, [rbp - {d}] \n", .{rhs_offset}),
+                        .integerLiteral => |rhs_value| try w.print("  shr rax, {d} \n", .{rhs_value}),
+                        else => {},
+                    }
+                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
                 },
                 .infix_plus => |infix_plus| {
                     const target_offset = infix_plus.offset;
@@ -148,86 +286,6 @@ pub fn build(self: @This(), ir: Ir) !void {
                     }
                     try w.print("  div rbx\n", .{});
                     try w.print("  mov QWORD [rbp - {d}], rdx\n", .{target_offset});
-                },
-                .infix_l => |infix| {
-                    try w.print("  xor rdx, rdx\n", .{});
-                    try loadReg(w, "  rax", infix.lhs);
-                    try loadReg(w, "  rbx", infix.rhs);
-                    try w.print("  cmp rax, rbx\n", .{});
-                    try w.print("  setl dl\n", .{});
-                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
-                },
-                .infix_le => |infix| {
-                    try w.print("  xor rdx, rdx\n", .{});
-                    try loadReg(w, "  rax", infix.lhs);
-                    try loadReg(w, "  rbx", infix.rhs);
-                    try w.print("  cmp rax, rbx\n", .{});
-                    try w.print("  setle dl\n", .{});
-                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
-                },
-                .infix_eq => |infix| {
-                    try w.print("  xor rdx, rdx\n", .{});
-                    try loadReg(w, "  rax", infix.lhs);
-                    try loadReg(w, "  rbx", infix.rhs);
-                    try w.print("  cmp rax, rbx\n", .{});
-                    try w.print("  sete dl\n", .{});
-                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
-                },
-                .infix_g => |infix| {
-                    try w.print("  xor rdx, rdx\n", .{});
-                    try loadReg(w, "  rax", infix.lhs);
-                    try loadReg(w, "  rbx", infix.rhs);
-                    try w.print("  cmp rax, rbx\n", .{});
-                    try w.print("  setg dl\n", .{});
-                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
-                },
-                .infix_ge => |infix| {
-                    try w.print("  xor rdx, rdx\n", .{});
-                    try loadReg(w, "  rax", infix.lhs);
-                    try loadReg(w, "  rbx", infix.rhs);
-                    try w.print("  cmp rax, rbx\n", .{});
-                    try w.print("  setge dl\n", .{});
-                    try w.print("  mov [rbp - {d}], rdx\n", .{infix.offset});
-                },
-                .infix_and => |infix| {
-                    const target_offset = infix.offset;
-                    try loadReg(w, "  rax", infix.lhs);
-                    switch (infix.rhs) {
-                        .variable => |rhs_offset| try w.print("  and rax, [rbp - {d}]\n", .{rhs_offset}),
-                        .integerLiteral => |rhs_value| try w.print("  and rax, {d} \n", .{rhs_value}),
-                        else => {},
-                    }
-                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
-                },
-                .infix_or => |infix| {
-                    const target_offset = infix.offset;
-                    try loadReg(w, "  rax", infix.lhs);
-                    switch (infix.rhs) {
-                        .variable => |rhs_offset| try w.print("  or rax, [rbp - {d}]\n", .{rhs_offset}),
-                        .integerLiteral => |rhs_value| try w.print("  or rax, {d} \n", .{rhs_value}),
-                        else => {},
-                    }
-                    try w.print("  mov [rbp - {d}], rax\n", .{target_offset});
-                },
-                .call => |call| {
-                    // try w.print("  ;function call\n", .{});
-                    const fnName = call.name;
-                    const registers = [_][]const u8{ "RDI", "RSI", "RDX", "RCX", "R8", "R9" };
-                    if (call.args.len <= registers.len) {
-                        for (call.args, 0..) |arg, i| {
-                            const reg = registers[i];
-                            switch (arg) {
-                                .variable => |offset| try w.print("  mov {s}, [rbp - {d}] \n", .{ reg, offset }),
-                                .integerLiteral => |value| try w.print("  mov {s}, {d} \n", .{ reg, value }),
-                                .dataLiteral => |value| try w.print("  mov {s}, data{d}+0 \n", .{ reg, value }),
-                            }
-                        }
-                        // TODO azzera il registo AL, serve per chiamara printf, non so se serve per tutte le funzioni extrn
-                        try w.print("  xor eax, eax\n", .{});
-                        try w.print("  call {s}\n", .{fnName});
-                    } else {
-                        @panic("Call a funzioni esterne supporta al massimo 6 argomenti");
-                    }
                 },
             }
         }
